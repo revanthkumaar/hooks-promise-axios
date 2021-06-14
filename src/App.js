0d05f37d78041cd848de05685d86a98d82744bbd
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-14 11:41:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+
+function GithubCommit() {
+  const [page, setPage] = useState(2);
+  const [commitHistory, setCommitHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const loadMoreCommit = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    
+    fetch(
+      `https://api.github.com/search/commits?q=repo:facebook/react+css&page=${page}`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Accept: "application/vnd.github.cloak-preview"
+        })
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        setCommitHistory(response.items);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error));
+  }, [page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> API calls with React Hooks </h1>
+      {isLoading && <p>Wait I'm Loading comments for you</p>}
+
+      {commitHistory.length !== 0 && (
+        <button onClick={loadMoreCommit}>Load More Commits</button>
+      )}
+
+      {commitHistory.map((c, index) => (
+        <div key={index}>
+          {c.commit && (
+            <>
+              <div>
+                <h2 style={{ textDecoration: "Underline" }}>
+                  {c.commit.committer.name}
+                </h2>
+                <p>{c.commit.message}</p>
+              </div>
+              <hr />
+            </>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default GithubCommit;
